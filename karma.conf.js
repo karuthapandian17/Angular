f0de9c03c760a4d1da51a088cd15e2a451bffb2b
karuthapandian17 <karuthapandian@nullerrors.com>
2025-11-17 19:23:24 +0530

    Add files via upload

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/task'),
+      dir: require('path').join(__dirname, './coverage/scroll'),
       subdir: '.',
       reporters: [
         { type: 'html' },
---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TaskFormComponent } from './task-form/task-form.component';
+import { TaskListComponent } from './task-list/task-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TaskFormComponent,
+    TaskListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
